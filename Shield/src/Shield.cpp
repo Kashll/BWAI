7e6dda901e33a465e3956c1100353cf4f9b6c304
Kashll <kashll@comcast.net>
2015-05-25 02:40:40 -0700

    First commit for Shield AI.

---
+++ Shield/src/Shield.cpp
@@ -0,0 +1,88 @@
+#include "Shield.h"
+
+using namespace BWAPI;
+
+void Shield::onStart()
+{
+	Broodwar->sendText("Shield Hello World!");
+}
+
+void Shield::onEnd(bool isWinner)
+{
+
+}
+
+void Shield::onFrame()
+{
+
+}
+
+void Shield::onSendText(std::string text)
+{
+
+}
+
+void Shield::onReceiveText(BWAPI::Player player, std::string text)
+{
+
+}
+
+void Shield::onPlayerLeft(BWAPI::Player player)
+{
+
+}
+
+void Shield::onNukeDetect(BWAPI::Position target)
+{
+
+}
+
+void Shield::onUnitDiscover(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitEvade(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitShow(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitHide(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitCreate(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitDestroy(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitMorph(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onUnitRenegade(BWAPI::Unit unit)
+{
+
+}
+
+void Shield::onSaveGame(std::string gameName)
+{
+
+}
+
+void Shield::onUnitComplete(BWAPI::Unit unit)
+{
+
+}
